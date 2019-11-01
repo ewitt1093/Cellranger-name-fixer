0a1e82a2d6cf8985eb5b62a3aa3fa4a14ba33d9b
ewitt1093 <41487088+ewitt1093@users.noreply.github.com>
2019-11-01 13:59:07 -0400

    Create fixer.bash
    Run this file as follows:
    bash fixer.bash [path to genes.tsv] [path to reference gtf]

---
+++ fixer.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+tsv=$1
+gtf=$2
+
+
+cp $tsv genesbackup.tsv
+cat  $gtf |grep FBgn | awk '{if ($3=="gene") print $10, $12}' | sed 's/"//g' | sed 's/;//g' |sort -u >genenames.txt
+
+
+Rscript merged_and_ordered.R genes.tsv genenames.txt genes2.tsv
+
+
+i=$(wc -l genesbackup.tsv|awk '{print $1}' )
+tail -n $i genes2.tsv | sed 's/"//g' | awk -v OFS='\t' '{if ($4 != "NA") print $2, $4; else if ($4 == "NA") print $2, $2 }' > genes3.tsv
+cp genes3.tsv genes.tsv
+rm genenames.txt
+rm genes2.tsv
+rm genes3.tsv
