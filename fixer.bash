5b90a690b1192ae95364c3fddcc0e7afe85d8b44
ewitt1093 <41487088+ewitt1093@users.noreply.github.com>
2019-11-01 15:59:37 -0400

    Update fixer.bash

--- fixer.bash
+++ fixer.bash
@@ -13,7 +13,7 @@ Rscript merged_and_ordered.R genes.tsv genenames.txt genes2.tsv
 
 i=$(wc -l genesbackup.tsv|awk '{print $1}' )
 tail -n $i genes2.tsv | sed 's/"//g' | awk -v OFS='\t' '{if ($4 != "NA") print $2, $4; else if ($4 == "NA") print $2, $2 }' > genes3.tsv
-cp genes3.tsv genes.tsv
+cp genes3.tsv $1
 rm genenames.txt
 rm genes2.tsv
 rm genes3.tsv
