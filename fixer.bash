40d6e62fd7fb53328c26a1e8403189b84a72911f
ewitt1093 <41487088+ewitt1093@users.noreply.github.com>
2021-02-24 10:02:06 -0500

    Update fixer.bash

--- fixer.bash
+++ fixer.bash
@@ -4,16 +4,16 @@ tsv=$1
 gtf=$2
 
 
-cp $tsv genesbackup.tsv
-cat  $gtf |grep FBgn | awk '{if ($3=="gene") print $10, $12}' | sed 's/"//g' | sed 's/;//g' |sort -u >genenames.txt
+cp $tsv featuresbackup.tsv
+cat  $gtf |grep FBgn | awk '{if ($3=="gene") print $10, $12}' | sed 's/"//g' | sed 's/;//g' |sort -u >featurenames.txt
 
 
-Rscript merged_and_ordered.R genes.tsv genenames.txt genes2.tsv
+Rscript merged_and_ordered.R features.tsv featurenames.txt features2.tsv
 
 
 i=$(wc -l genesbackup.tsv|awk '{print $1}' )
-tail -n $i genes2.tsv | sed 's/"//g' | awk -v OFS='\t' '{if ($4 != "NA") print $2, $4; else if ($4 == "NA") print $2, $2 }' > genes3.tsv
-cp genes3.tsv $1
-rm genenames.txt
-rm genes2.tsv
-rm genes3.tsv
+tail -n $i genes2.tsv | sed 's/"//g' | awk -v OFS='\t' '{if ($4 != "NA") print $2, $4; else if ($4 == "NA") print $2, $2 }' > features3.tsv
+cp features3.tsv $1
+rm featurenames.txt
+rm features2.tsv
+rm features3.tsv
