db2a1a036ac8f7ab08e24198af2c4761a8700ad5
ewitt1093 <41487088+ewitt1093@users.noreply.github.com>
2021-02-24 11:06:21 -0500

    Update fixer.bash

--- fixer.bash
+++ fixer.bash
@@ -9,11 +9,3 @@ cat  $gtf |grep FBgn | awk '{if ($3=="gene") print $10, $12}' | sed 's/"//g' | s
 
 
 Rscript merged_and_ordered.R features.tsv featurenames.txt features2.tsv
-
-
-i=$(wc -l featuresbackup.tsv|awk '{print $1}' )
-tail -n $i features2.tsv | sed 's/"//g' | awk -v OFS='\t' '{if ($4 != "NA") print $2, $4; else if ($4 == "NA") print $2, $2 }' > features3.tsv
-cp features3.tsv $1
-rm featurenames.txt
-rm features2.tsv
-rm features3.tsv
