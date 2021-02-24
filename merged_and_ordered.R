0282efe394de2c5abd50dc72811d59e7b7ca650f
ewitt1093 <41487088+ewitt1093@users.noreply.github.com>
2021-02-24 12:11:52 -0500

    Update merged_and_ordered.R

--- merged_and_ordered.R
+++ merged_and_ordered.R
@@ -6,4 +6,5 @@ args = commandArgs(trailingOnly=TRUE)
 features<-join(features, names, by="V1")
 features<-features[,c(1,5,3)]
 features$V3<-"Gene Expression"
+features$V2 <- ifelse(is.na(features$V2), features$V1, features$V2)
 write.table(features, file=args[3], quote=FALSE, sep='\t', col.names = NA, row.names=NA)
