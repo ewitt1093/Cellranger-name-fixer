c57876f4c3a56af9e6eee8f292cef87580f76b07
ewitt1093 <41487088+ewitt1093@users.noreply.github.com>
2021-02-24 10:02:39 -0500

    Update merged_and_ordered.R

--- merged_and_ordered.R
+++ merged_and_ordered.R
@@ -1,9 +1,9 @@
 #!/usr/bin/env Rscript
 library(plyr)
 args = commandArgs(trailingOnly=TRUE)
- genes <-read.table(args[1])
+ features <-read.table(args[1])
  names <-read.table(args[2])
-genes<-join(genes, names, by="V1")
-genes<-genes[,c(1,5,3)]
-genes$V3<-"Gene Expression"
-write.table(genes, file=args[3])
+features<-join(features, names, by="V1")
+features<-features[,c(1,5,3)]
+features$V3<-"Gene Expression"
+write.table(features, file=args[3])
