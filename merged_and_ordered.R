278ac12ef6bcdfb86eff81d2ca1b61ee5e77880d
ewitt1093 <41487088+ewitt1093@users.noreply.github.com>
2021-04-15 09:57:28 -0400

    Update merged_and_ordered.R

--- merged_and_ordered.R
+++ merged_and_ordered.R
@@ -2,9 +2,16 @@
 library(plyr)
 args = commandArgs(trailingOnly=TRUE)
  features <-read.table(args[1])
+names(features)<-c("ID","ID2")
  names <-read.table(args[2])
-features<-join(features, names, by="V1")
-features<-features[,c(1,5,3)]
-features$V3<-"Gene Expression"
-features$V2 <- ifelse(is.na(features$V2), features$V1, features$V2)
+names(names)<-c("ID","gene")
+features<-join(features, names, by="ID")
+features<-features[,c("ID","gene")]
+features$type<-"Gene Expression"
+
+library(dplyr)
+
+features <- features %>% mutate(gene = coalesce(gene, ID))
+
+#features$gene <- ifelse(is.na(features$gene), features$ID, features$gene)
 write.table(features, file=args[3], quote=FALSE, sep='\t', col.names = F, row.names=F)
