8db81702ca96591fc85ec63a8b63bdd77fd1cb09
ewitt1093 <41487088+ewitt1093@users.noreply.github.com>
2021-04-20 08:59:08 -0400

    Update fixer.bash

--- fixer.bash
+++ fixer.bash
@@ -8,4 +8,4 @@ cp $tsv featuresbackup.tsv
 cat  $gtf |grep FBgn | awk '{if ($3=="gene") print $10, $12}' | sed 's/"//g' | sed 's/;//g' |sort -u >featurenames.txt
 
 
-Rscript merged_and_ordered.R features.tsv featurenames.txt features2.tsv
+Rscript merged_and_ordered.R $1 featurenames.txt features2.tsv
